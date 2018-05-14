9d0b44a19166c98cb43a75448c92bbf1b0dd8bb5
Sindri Guðmundsson <sindrig@tempo.io>
2018-05-14 10:56:02 +0000

    Avoid uploading same file

--- match-results/uploader.py
+++ match-results/uploader.py
@@ -1,5 +1,7 @@
 import os
+import hashlib
 import boto3
+import botocore
 
 DEBUG = os.getenv('DEBUG')
 
@@ -10,15 +12,38 @@ session = boto3.Session(**kwargs)
 client = session.client('s3')
 
 
+def get_md5sum(data):
+    md5 = hashlib.md5()
+    md5.update(data)
+    return md5.hexdigest()
+
+
 def upload_file(s3_bucket, key, html, content_type='text/html; charset=utf-8'):
-    kwargs = dict(
-        Body=html,
+    html = html.encode('utf8')
+    md5 = get_md5sum(html)
+    head_kwargs = dict(
         Bucket=s3_bucket,
         Key=key,
-        ContentType=content_type,
     )
-    if DEBUG:
-        with open('/home/sindri/vikes-result/%s' % (key, ), 'w') as f:
-            f.write(kwargs['Body'])
+    kwargs = dict(
+        Body=html,
+        ContentType=content_type,
+        # ContentMD5=md5,
+        **head_kwargs
+    )
+    try:
+        client.head_object(IfNoneMatch=md5, **head_kwargs)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '304':
+            print('No change in file %s/%s' % (s3_bucket, key, ))
             return
+        else:
+            raise e
+    if DEBUG:
+        fn = '/home/sindri/vikes-result/%s' % (key, )
+        print('Writing %s/%s to %s' % (s3_bucket, key, fn,))
+        with open(fn, 'w') as f:
+            f.write(kwargs['Body'].decode('utf8'))
+            return
+    print('Uploading %s/%s' % (s3_bucket, key, ))
     client.put_object(**kwargs)
