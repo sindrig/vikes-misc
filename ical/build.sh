6f6ef2e271888e24ac3d828bcdb7c8abc165bf6c
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-06-30 14:24:10 +0000

    Update for 2020

--- ical/build.sh
+++ ical/build.sh
@@ -11,8 +11,8 @@ if [ "$KVK_OUT" == "" ]; then
     exit 1
 fi
 
-python create_ical.py 103 $REG_OUT 1.4.2019 30.11.2019 1 --name="Pepsi Max 2019 - Víkingur"
-python create_ical.py 103 $VIP_OUT 1.4.2019 30.11.2019 1 --name="Pepsi Max 2019 - Víkingur VIP"\
+python create_ical.py 103 $REG_OUT 1.4.2020 30.11.2020 1 --name="Pepsi Max 2020 - Víkingur"
+python create_ical.py 103 $VIP_OUT 1.4.2020 30.11.2020 1 --name="Pepsi Max 2020 - Víkingur VIP"\
     --filter_by="self.home=='Víkingur R.'"\
     --event_edit="self.summary+=' VIP';self.date_end=self.date_start;self.date_start-=datetime.timedelta(hours=1)"
-python create_ical.py 103 $KVK_OUT 1.4.2019 30.11.2019 0 --name="Pepsi Max 2019 - HK/Víkingur"
+python create_ical.py 103 $KVK_OUT 1.4.2020 30.11.2020 0 --name="Pepsi Max 2020 - HK/Víkingur"
