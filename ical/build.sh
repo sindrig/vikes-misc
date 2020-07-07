b92fec752197b94fafcb443cc8289268c16de992
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-07-07 09:36:48 +0000

    Update because no HK

--- ical/build.sh
+++ ical/build.sh
@@ -15,4 +15,4 @@ python create_ical.py 103 $REG_OUT 1.4.2020 30.11.2020 1 --name="Pepsi Max 2020 
 python create_ical.py 103 $VIP_OUT 1.4.2020 30.11.2020 1 --name="Pepsi Max 2020 - Víkingur VIP"\
     --filter_by="self.home=='Víkingur R.'"\
     --event_edit="self.summary+=' VIP';self.date_end=self.date_start;self.date_start-=datetime.timedelta(hours=1)"
-python create_ical.py 103 $KVK_OUT 1.4.2020 30.11.2020 0 --name="Pepsi Max 2020 - HK/Víkingur"
+python create_ical.py 103 $KVK_OUT 1.4.2020 30.11.2020 0 --name="Lengjudeildin 2020 - Víkingur"
